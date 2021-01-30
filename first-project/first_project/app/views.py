9c4331f1c013cdc67fc28de2eb880849d0800989
DamnFilthy <aleksandr-shiryaev-1993@mail.ru>
2021-01-30 10:36:50 +0300

    updated

--- first-project/first_project/app/views.py
+++ first-project/first_project/app/views.py
@@ -6,8 +6,6 @@ import os
 
 def home_view(request):
     template_name = 'app/home.html'
-    # впишите правильные адреса страниц, используя
-    # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
         'Показать текущее время': reverse('time'),
@@ -15,8 +13,6 @@ def home_view(request):
         'Тестовая страница': reverse('test')
     }
 
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
     context = {
         'pages': pages
     }
@@ -32,10 +28,6 @@ def time_view(request):
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
-    # raise NotImplemented
     folders = os.listdir(path='.')
     result = []
     for folder in folders:
